8f2e82b0f995ce3f91b32d96e0e147fdd69ec165
Kevin Randles <krandles@gmail.com>
2017-11-02 02:45:23 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,6 +5,12 @@ function getIt() {
   })
 }
 
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
